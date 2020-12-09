38955c19cc9e75a5472f97c5cba0d5e70fe1eda1
freehere107 <freehere107@gmail.com>
2020-12-09 14:01:17 +0800

    mainnet conf and fix drill transfer

--- src/objectOwnership.ts
+++ src/objectOwnership.ts
@@ -1,21 +1,20 @@
-import {BigInt, BigDecimal} from "@graphprotocol/graph-ts"
+import {BigInt, BigDecimal, Address} from "@graphprotocol/graph-ts"
 
 import {
     Transfer,
 } from "../generated/ObjectOwnership/Contract"
-import {NFTTransfer} from "../generated/schema"
+import {NFTTransfer, Drill} from "../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
     let transfer = new NFTTransfer(event.transaction.hash.toHex())
     transfer.from = event.params._from
     transfer.to = event.params._to
     transfer.tokenId = event.params._tokenId.toHex()
-    transfer.nftType = getAssetTypeByTokenId(event.params._tokenId.toHex())
+    transfer.nftType = getAssetTypeByTokenId(transfer.tokenId, event.params._to)
     transfer.save()
 }
 
-
-function getAssetTypeByTokenId(tokenId: String): string {
+function getAssetTypeByTokenId(tokenId: string, to: Address): string {
     // @ts-ignore
     // todo need use tokenId >>192 && 255
     switch (parseI32(tokenId.substr(17, 1))) {
@@ -26,6 +25,11 @@ function getAssetTypeByTokenId(tokenId: String): string {
         case 3:
             return "MirrorKitty"
         case 4:
+            let drill = Drill.load(tokenId)
+            if (drill !== null) {
+                drill.owner = to
+                drill.save()
+            }
             return "Drill"
         default:
             return ""
